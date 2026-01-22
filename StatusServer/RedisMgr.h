46c4605d96c776b3d59c505ce5c6dbd80384210a
Mahiro <oyamahiro811@gmail.com>
2026-01-22 22:37:12 +0800

    修复RedisMgr的bug

--- StatusServer/RedisMgr.h
+++ StatusServer/RedisMgr.h
@@ -32,13 +32,31 @@ public:
 		std::cout << "认证成功" << std::endl;
 		connections_.push(context);
 	  }
+
+	  check_thread_ = std::thread([this]() {
+		  while (!b_stop_) {
+			  counter_++;
+			  if (counter_ >= 60) {
+				  checkThread();
+				  counter_ = 0;
+			  }
+
+			  std::this_thread::sleep_for(std::chrono::seconds(1)); // 每隔 30 秒发送一次 PING 命令
+		  }
+		  });
+  }
+
+  void ClearConnections() {
+	  std::lock_guard<std::mutex> lock(mutex_);
+	  while (!connections_.empty()) {
+		  auto* context = connections_.front();
+		  redisFree(context);
+		  connections_.pop();
+	  }
   }
 
   ~RedisConPool() {
-	std::lock_guard<std::mutex> lock(mutex_);
-	while (!connections_.empty()) {
-	  connections_.pop();
-	}
+	  
   }
 
   redisContext* getConnection() {
@@ -73,13 +91,61 @@ public:
   }
 
 private:
+	void checkThread() {
+		std::lock_guard<std::mutex> lock(mutex_);
+		if (b_stop_) {
+			return;
+		}
+		auto pool_size = connections_.size();
+		for (int i = 0; i < pool_size && !b_stop_; i++) {
+			auto* context = connections_.front();
+			connections_.pop();
+			try {
+				auto reply = (redisReply*)redisCommand(context, "PING");
+				if (!reply) {
+					std::cout << "reply is null, redis ping failed: " << std::endl;
+					connections_.push(context);
+					continue;
+				}
+				freeReplyObject(reply);
+				connections_.push(context);
+			}
+			catch (std::exception& exp) {
+				std::cout << "Error keeping connection alive: " << exp.what() << std::endl;
+				redisFree(context);
+				context = redisConnect(host_, port_);
+				if (context == nullptr || context->err != 0) {
+					if (context != nullptr) {
+						redisFree(context);
+					}
+					continue;
+				}
+
+				auto reply = (redisReply*)redisCommand(context, "AUTH %s", pwd_);
+				if (reply->type == REDIS_REPLY_ERROR) {
+					std::cout << "认证失败" << std::endl;
+					//执行成功 释放redisCommand执行后返回的redisReply所占用的内存
+					freeReplyObject(reply);
+					continue;
+				}
+
+				//执行成功 释放redisCommand执行后返回的redisReply所占用的内存
+				freeReplyObject(reply);
+				std::cout << "认证成功" << std::endl;
+				connections_.push(context);
+			}
+		}
+	}
   std::atomic<bool> b_stop_;
   size_t poolSize_;
   const char* host_;
+  const char* pwd_;
   int port_;
   std::queue<redisContext*> connections_;
   std::mutex mutex_;
   std::condition_variable cond_;
+  std::thread  check_thread_;
+  int counter_;
 };
 
 class RedisMgr:public Singleton<RedisMgr>, public std::enable_shared_from_this<RedisMgr>{
@@ -87,19 +153,18 @@ class RedisMgr:public Singleton<RedisMgr>, public std::enable_shared_from_this<R
 public:
   ~RedisMgr();
   void Close();
-  bool Connect(const std::string& host, int port);
   bool Get(const std::string& key, std::string& value);
   bool Set(const std::string& key, const std::string& value);
-  bool Auth(const std::string& password);
   bool LPush(const std::string& key, const std::string& value);
   bool LPop(const std::string& key, std::string& value);
   bool RPush(const std::string& key, const std::string& value);
   bool RPop(const std::string& key, std::string& value);
   bool HSet(const std::string& key, const std::string& hkey, const std::string& value);
-  bool HSet(const char* key, const char* hkey, const char* hvalue, size_t hvaluelen); 
+  bool HSet(const char* key, const char* hkey, const char* hvalue, size_t hvaluelen);
+  std::string HGet(const std::string& key, const std::string& hkey);
+  bool HDel(const std::string& key, const std::string& field);
   bool Del(const std::string& key);
   bool ExistsKey(const std::string& key);
-  auto HGet(const std::string& key, const std::string& hkey) -> std::string;
 
 private:
   RedisMgr();
